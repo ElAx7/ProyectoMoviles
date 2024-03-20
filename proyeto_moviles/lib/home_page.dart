cdfb0adfd949a0441060671d3797c27899c48dcc
ElAx7 <125324367+ElAx7@users.noreply.github.com>
2024-03-19 20:17:53 -0600

    registration window

--- proyeto_moviles/lib/home_page.dart
+++ proyeto_moviles/lib/home_page.dart
@@ -1,18 +1,89 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material App Bar'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String _email = "";
+  String _password = "";
+  bool _rememberMe = false;
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(32),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Bienvenido",
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              labelText: "Correo",
+            ),
+            onChanged: (value) {
+              setState(() {
+                _email = value;
+              });
+            },
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              labelText: "Contraseña",
+            ),
+            onChanged: (value) {
+              setState(() {
+                _password = value;
+              });
+            },
+            obscureText: true,
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              Checkbox(
+                value: _rememberMe,
+                onChanged: (value) {
+                  setState(() {
+                    _rememberMe = value!;
+                  });
+                },
+              ),
+              Text("Recuérdame"),
+            ],
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              // Aquí podrías implementar la lógica de inicio de sesión
+              // utilizando los valores de _email, _password y _rememberMe
+            },
+            child: Text("Iniciar Sesión"),
+          ),
+        ],
       ),
     );
   }
