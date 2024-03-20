ab5209abc06e0d43a33cfb693c5e45d1eff896fa
ElAx7 <125324367+ElAx7@users.noreply.github.com>
2024-03-19 20:22:22 -0600

    loginPage

--- proyeto_moviles/lib/main.dart
+++ proyeto_moviles/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:proyeto_moviles/home_page.dart';
+import 'package:proyeto_moviles/login_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
