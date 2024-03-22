1c3ed1f6353c19004734cfb3ecc4bbf58cd11a9f
Rigo A Glez <157529375+Yocomovez@users.noreply.github.com>
2024-03-22 16:59:47 -0600

    new_list.dart

---
+++ proyeto_moviles/lib/new_list.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:proyeto_moviles/next.dart';
+
+class new_list extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Nueva Lista')),
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+            Navigator.pop(context); // Cierra la pantalla actual
+          },
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Center(
+              // Centrar el texto
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Llena los campos para generar una nueva lista',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Título'),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(labelText: 'Descripción'),
+            ),
+            Expanded(
+                child: Container()), // Espacio para expandir y centrar el botón
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => next()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                padding: EdgeInsets.symmetric(vertical: 15.0),
+              ),
+              child: Text(
+                'Crear >',
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
